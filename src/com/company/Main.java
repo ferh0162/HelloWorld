47fe4b8cbab1a7ed1d6c42285378258b8eb766cd
ferh0162 <ferh0162@stud.kea.dk>
2022-02-23 14:46:16 +0100

    "First Commit"

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        // Hvad skal der ske shababs, mit navn er Ferhat og jeg tester min nye macbook af i intellij
+        System.out.println("Hello, Mac");
+
+
+    }
+}
